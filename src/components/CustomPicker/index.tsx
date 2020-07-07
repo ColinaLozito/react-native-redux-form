a7b125adf6b2d44bd28b879598135b84dd7281e5
Dicoloze <luisdicoloze@gmail.com>
2020-07-07 12:02:41 +0200

    picker modal refactoir

--- src/components/CustomPicker/index.tsx
+++ src/components/CustomPicker/index.tsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
 import {
-    Modal,
-    TouchableWithoutFeedback,
     Text,
     View,
-    Picker,
     TextInput,
     TouchableOpacity,
 } from 'react-native';
-import styles from './styles';
+import PickerModal from './PickerModal';
 
+import styles from './styles';
 
 interface Props {
     items: Array<any>
@@ -18,46 +16,8 @@ interface Props {
 }
 
 const CustomPicker: React.FC<Props> = ({ items, label, warn }): any => {
-    const [value, setValue] = useState<string>('');
     const [visible, setVisible] = useState<boolean>(false);
-
-    const pickerModal = () => (
-        <Modal
-            animationType="slide"
-            transparent={true}
-            visible={visible}>
-            <TouchableWithoutFeedback
-                onPress={() => setVisible(true)}
-            >
-                <View style={styles.modalContainer}>
-                    <View style={styles.buttonContainer}>
-                        <Text
-                            style={styles.done}
-                            onPress={() => setVisible(false)}
-                        >
-                            Done
-                        </Text>
-                    </View>
-                    <View style={styles.picker}>
-                        <Picker
-                            selectedValue={value}
-                            onValueChange={(country: string) => {
-                                setValue(country)
-                            }}
-                        >
-                            {items.map((i, index) => (
-                                <Picker.Item
-                                    key={index}
-                                    label={i.label}
-                                    value={i.value}
-                                />
-                            ))}
-                        </Picker>
-                    </View>
-                </View>
-            </TouchableWithoutFeedback>
-        </Modal>
-    );
+    const [labelSelected, setLabelSelected] = useState<string>('');
 
     return (
         <View style={styles.container}>
@@ -67,10 +27,17 @@ const CustomPicker: React.FC<Props> = ({ items, label, warn }): any => {
                     style={[styles.inputBox, warn ? styles.warn : styles.regular]}
                     placeholder={label}
                     editable={false}
+                    defaultValue={labelSelected}
                 />
                 <TouchableOpacity style={styles.touchable} onPress={() => setVisible(true)} />
             </View>
-            {pickerModal()}
+            <PickerModal
+                visible={visible}
+                value={labelSelected}
+                setVisible={setVisible}
+                items={items}
+                onSelect={setLabelSelected}
+            />
         </View>
     );
 
