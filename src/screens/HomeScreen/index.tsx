9365e76d161dd41933dd57c44ee96f23a17fc380
Dicoloze <luisdicoloze@gmail.com>
2020-07-08 00:55:15 +0200

    get countries to form

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
   TouchableOpacity,
   Keyboard,
 } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 import Form from '../../components/Form';
 
 import styles from './styles';
 
-const items = [
+const inputItems = [
   {
     fieldType: 'input',
     label: 'Social Secutiry Number',
@@ -22,7 +23,7 @@ const items = [
   },
   {
     fieldType: 'input',
-    label: 'E-Mail',
+    label: 'E-mail Address',
     type: 'email',
   },
   {
@@ -32,53 +33,34 @@ const items = [
   },
 ];
 
+export default (): JSX.Element => {
+  const [countriesList, setCountriesList] = useState<Array<any>>([]);
 
-const programmingLanguages = [
-  {
-    label: 'Java',
-    value: 'java',
-  },
-  {
-    label: 'JavaScript',
-    value: 'JavaScript',
-  },
-  {
-    label: 'Python',
-    value: 'python',
-  },
-  {
-    label: 'Ruby',
-    value: 'ruby',
-  },
-  {
-    label: 'C#',
-    value: 'csharp',
-  },
-  {
-    label: 'C++',
-    value: 'cpp',
-  },
-  {
-    label: 'C',
-    value: 'c',
-  },
-  {
-    label: 'Go',
-    value: 'go',
-  },
-];
+  const onGetCountries = async () => {
+    const countries = await SecureStore.getItemAsync('countries');
+    if (countries) {
+      setCountriesList(JSON.parse(countries));
+    } else {
+      setCountriesList([]);
+    }
+  };
 
-export default (): JSX.Element => (
-  <View style={styles.container}>
-    <TouchableOpacity
-      style={styles.contentContainer}
-      activeOpacity={1}
-      onPress={() => Keyboard.dismiss()}
-    >
-      <View style={styles.titleWrapper}>
-        <Text style={styles.formTitle}>REDUX PERSIST FORM</Text>
-      </View>
-      <Form inputItems={items} selectItems={programmingLanguages} />
-    </TouchableOpacity>
-  </View>
-);
+  useEffect(() => {
+    onGetCountries();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.contentContainer}
+        activeOpacity={1}
+        onPress={() => Keyboard.dismiss()}
+      >
+        <View style={styles.titleWrapper}>
+          <Text style={styles.formTitle}>REDUX PERSIST FORM</Text>
+        </View>
+        <Form inputItems={inputItems} selectItems={countriesList} />
+      </TouchableOpacity>
+    </View>
+  );
+};
