b0768f86d28755da811ca620342b7e182b2d24c6
Dicoloze <luisdicoloze@gmail.com>
2020-07-07 22:45:07 +0200

    form improved

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 import {
     View,
 } from 'react-native';
+import { connect } from 'react-redux';
 import Input from '../../lib/components/Input';
 import Button from '../../lib/components/Button';
 import CustomPicker from '../../components/CustomPicker';
+import { getFormState, saveForm, ereaseForm } from '../../store/action';
+
+import formData from './formData';
+
 import styles from './styles';
+
+interface Form {
+    ssn?: string | null | undefined,
+    phone?: string | null | undefined,
+    email?: string | null | undefined,
+    country?: string | null | undefined
+}
 
 interface Props {
     selectItems: Array<any>,
     inputItems: Array<any>
+    form?: any | Form,
+    onSubmit?: () => any,
+    onSaveForm?: () => any
 }
 
 interface InputItem {
@@ -18,28 +33,47 @@ interface InputItem {
     label: string
 }
 
-const Form: React.FC<Props> = ({ selectItems, inputItems }): any => {
+const Form: React.FC<Props> = ({ selectItems, inputItems, form, onSubmit, onSaveForm }): any => {
 
-    const InputList = (): any => inputItems.map((item: InputItem, pos: number) => {
+    const [formValues, handleFormValueChange] = formData({});
+
+    const inputList = (): any => inputItems.map((item: InputItem, pos: number) => {
         if (item.fieldType === 'input') {
-            return <Input key={pos} label={item.label} type={item.type} />;
+            return (
+                <Input
+                    key={pos}
+                    label={item.label}
+                    type={item.type}
+                    value={form[item.type] || formValues[item.type]}
+                    handleFormValueChange={handleFormValueChange}
+                />
+            );
         }
         return <CustomPicker
             key={pos}
             label={item.label}
             items={selectItems}
             warn={false}
+            type={item.type}
+            value={form[item.type] || formValues[item.type]}
+            handleFormValueChange={handleFormValueChange}
         />;
     });
 
     return (
         <View style={styles.formContainer}>
             <View style={styles.inputWrapper}>
-                <InputList />
+                {inputList()}
             </View>
-            <Button label="submit" onSubmit={() => null} />
+            <Button label="submit" onSubmit={onSubmit} />
         </View>
     );
 };
 
-export default Form;
+export default connect(
+    getFormState,
+    {
+        onSubmit: ereaseForm,
+        onSaveForm: saveForm,
+    }
+)(Form);
