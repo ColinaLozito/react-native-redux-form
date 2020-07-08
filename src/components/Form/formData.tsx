3643df8aec7d6d8b87b91d93d6b1cfa5561cd0d4
Dicoloze <luisdicoloze@gmail.com>
2020-07-08 16:30:20 +0200

    form core logic adjustments

--- src/components/Form/formData.tsx
+++ src/components/Form/formData.tsx
@@ -1,28 +1,81 @@
 import { useState } from 'react';
+import { isValidEmail, isValidPhone, isValidSwedishSSN } from '../../lib/validators';
 
-export default (values: any, onSaveForm: ((obj: any) => any)) => {
+export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
     const [formValues, setFormValues] = useState<any>({
-        ...values,
+        ...form,
     });
 
-    const handleFormValueChange = (key: string, value: string) => {
+    const inputValidation = (type: string, value: string): boolean => {
+        switch (type) {
+            case 'ssn':
+                return isValidSwedishSSN(value);
+            case 'phone':
+                return isValidPhone(value);
+            case 'email':
+                return isValidEmail(value);
+            default:
+                return true;
+        }
+    };
+
+    const handleFormValueChange = (key: string, value: string): any => {
         onSaveForm(
             {
                 ...formValues,
-                [key]: value,
+                [key]: {
+                    value,
+                    valid: null,
+                },
             }
         );
         setFormValues(
             {
                 ...formValues,
-                [key]: value,
+                [key]: {
+                    value,
+                    valid: null,
+                },
             }
         );
+    };
+
+    const handleFormValidate = (key: string, value: string) => {
+        setFormValues(
+            {
+                ...formValues,
+                [key]: {
+                    value,
+                    valid: inputValidation(key, value),
+                },
+            }
+        );
+    };
+
+    const maxInputLengh = (type: string): number => {
+        switch (type) {
+            case 'ssn':
+                return 12;
+            case 'phone':
+                return 9;
+            default:
+                return 35;
+        }
+    };
+
+    const keyboardType = (type: string): string => {
+        if (type === 'phone' || type === 'ssn') {
+            return 'number-pad';
+        }
+        return 'default';
     };
 
     return [
         formValues,
         handleFormValueChange,
         setFormValues,
+        handleFormValidate,
+        maxInputLengh,
+        keyboardType,
     ];
 };
