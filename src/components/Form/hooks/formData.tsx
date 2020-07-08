83b5ca8c2dcc8fd09fc3ae315dc4fa8cae230367
Dicoloze <luisdicoloze@gmail.com>
2020-07-08 21:23:56 +0200

    form data hook relocated

--- src/components/Form/hooks/formData.tsx
+++ src/components/Form/hooks/formData.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
-import { isValidEmail, isValidPhone, isValidSwedishSSN } from '../../lib/validators';
+import {
+    isValidEmail,
+    isValidPhone,
+    isValidSwedishSSN,
+    isFormReadyToSubmit,
+} from '../../../lib/validators';
 
-export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
-    const [formValues, setFormValues] = useState<any>({
-        ...form,
-    });
+export default (onSaveForm: ((obj: any) => any), onSubmit: (() => any), form: any, inputItems: any) => {
+    const [formValues, setFormValues] = useState<any>({ ...form });
+    const [showErrorModal, setShowErrorModal] = useState<boolean>(false);
+    const [success, setSuccess] = useState<boolean | null>(null);
 
+    // validate input content depending of the type
     const inputValidation = (type: string, value: string): boolean => {
         switch (type) {
             case 'ssn':
@@ -18,8 +24,9 @@ export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
                 return true;
         }
     };
-
+    // on input change set value to the form state
     const handleFormValueChange = (key: string, value: string): any => {
+        // send object to REDUX store
         onSaveForm(
             {
                 ...formValues,
@@ -29,17 +36,18 @@ export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
                 },
             }
         );
+        // set imput values into form state
         setFormValues(
             {
                 ...formValues,
                 [key]: {
                     value,
-                    valid: null,
+                    valid: key === 'country' ? true : null,
                 },
             }
         );
     };
-
+    // validate input value and set it into form state
     const handleFormValidate = (key: string, value: string) => {
         setFormValues(
             {
@@ -51,8 +59,8 @@ export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
             }
         );
     };
-
-    const maxInputLengh = (type: string): number => {
+    // get max input length based on the type is the input
+    const maxInputLength = (type: string): number => {
         switch (type) {
             case 'ssn':
                 return 12;
@@ -62,20 +70,38 @@ export default (values: any, onSaveForm: ((obj: any) => any), form: any) => {
                 return 35;
         }
     };
-
+    // select keyboard type based on the type
     const keyboardType = (type: string): string => {
         if (type === 'phone' || type === 'ssn') {
             return 'number-pad';
         }
         return 'default';
     };
+    // handle submit button functionality
+    const redyToSubmit = (): any => {
+        delete formValues._persist;
+        if (isFormReadyToSubmit({ formValues, inputItems })) {
+            // if is good to submit
+            setFormValues({});
+            onSubmit();
+            setSuccess(true);
+            console.log('Success');
+        } else {
+            // if is false
+            setSuccess(false);
+        }
+        setShowErrorModal(true);
+    };
 
     return [
+        success,
         formValues,
+        showErrorModal,
         handleFormValueChange,
-        setFormValues,
         handleFormValidate,
-        maxInputLengh,
+        maxInputLength,
         keyboardType,
+        redyToSubmit,
+        setShowErrorModal,
     ];
 };
